e6638a7b041cf97ae0625e54bfcb43c00e15ac9d
gastonpena3 <gastonpena3@gmail.com>
2023-02-05 12:59:04 -0300

    Eureka create project
    first commit

---
+++ Eureka_picture_challege/Eureka_picture_challege/Eureka_picture_challegeApp.swift
@@ -0,0 +1,20 @@
+//
+//  Eureka_picture_challegeApp.swift
+//  Eureka_picture_challege
+//
+//  Created by Gastón Pena on 05/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct Eureka_picture_challegeApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
